8031b281ae2e7140cf3e43d2065e37aa90db1118
benburtt <bnlbrt@gmail.com>
2015-02-01 16:15:15 -0500

    Added new classes for gyro and updated robot.java

--- src/org/usfirst/frc/team151/robot/Robot.java
+++ src/org/usfirst/frc/team151/robot/Robot.java
@@ -4,6 +4,7 @@ import org.nashua.tt151.F310;
 import org.nashua.tt151.system.TankDrive;
 
 import edu.wpi.first.wpilibj.Gyro;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -11,9 +12,12 @@ public class Robot extends SampleRobot {
 	F310 driver = new F310( 0, 0.1 );
 	F310 lifter = new F310( 1, 0.1 );
 	Gyro gyro = new Gyro( 0 );
+	private GyroITG3200 m_gyro;
 	
 	public Robot() {
-		
+		//creates and initializes the gyro
+		m_gyro = new GyroITG3200(I2C.Port.kOnboard);
+    	m_gyro.initialize();
 	}
 	
 	public void autonomous() {}
@@ -41,6 +45,9 @@ public class Robot extends SampleRobot {
     	gyro.reset();
         while (isTest() && isEnabled()) {
         	SmartDashboard.putString("DB/String 0", Double.toString(gyro.getAngle()));
+        	SmartDashboard.putString("DB/String 1", Double.toString(m_gyro.getRotationX()));
+        	SmartDashboard.putString("DB/String 2", Double.toString(m_gyro.getRotationY()));
+        	SmartDashboard.putString("DB/String 3", Double.toString(m_gyro.getRotationZ()));
         }
     }
 	
