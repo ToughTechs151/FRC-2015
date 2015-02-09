0652bf1bee3effbbc49a812dbdd235ec17f1981c
benburtt <bnlbrt@gmail.com>
2015-02-09 17:40:57 -0500

    Revert "switch to robotdrive class for mecanum drive"
    This reverts commit c134b0ac341a67ace81d5f0e4c9f46588cdc7da7.
    
    Conflicts:
    	RobotTest/src/org/usfirst/frc/team151/robot/Robot.java

--- RobotTest/src/org/usfirst/frc/team151/robot/Robot.java
+++ RobotTest/src/org/usfirst/frc/team151/robot/Robot.java
@@ -10,8 +10,12 @@ import edu.wpi.first.wpilibj.SampleRobot;
 public class Robot extends SampleRobot {
 	F310 driver = new F310( 0, 0.1 );
 	F310 lifter = new F310( 1, 0.1 );
+<<<<<<< HEAD
 //	Gyro gyro = new Gyro( 0 );
 	RobotDrive myDrive = new RobotDrive(0, 2, 1, 3);
+=======
+	Gyro gyro = new Gyro( 0 );
+>>>>>>> parent of c134b0a... switch to robotdrive class for mecanum drive
 	
 	public Robot() {
 		
@@ -26,17 +30,17 @@ public class Robot extends SampleRobot {
 	}
 	
 	public void test() {
-//		gyro.reset();
-//   	gyro.setSensitivity(12.5/1000);
-//    	gyro.initGyro();
+		gyro.reset();
+    	gyro.setSensitivity(12.5/1000);
+    	gyro.initGyro();
     	try {
 			Thread.sleep(10);
 		} catch (InterruptedException e) {
 			System.out.println(e.getMessage());
 		}
-//    	gyro.reset();
+    	gyro.reset();
         while (isTest() && isEnabled()) {
-//        	SmartDashboard.putString("DB/String 0", Double.toString(gyro.getAngle()));
+        	SmartDashboard.putString("DB/String 0", Double.toString(gyro.getAngle()));
         }
     }
 	
