542e8594ad86d2605b42b1bca10c331c906ee480
benburtt <bnlbrt@gmail.com>
2015-02-17 22:44:54 -0500

    Changed lifter class to work with second joystick

--- FRC 2015/src/org/nashua/tt151/system/Lifter.java
+++ FRC 2015/src/org/nashua/tt151/system/Lifter.java
@@ -1,6 +1,7 @@
 package org.nashua.tt151.system;
 
 import org.nashua.tt151.F310;
+import org.nashua.tt151.MathUtils;
 import org.nashua.tt151.RobotMap;
 
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
@@ -32,16 +33,17 @@ public class Lifter extends Subsystem {
 	
 	@Override
 	public void operatorControl( F310 driver, F310 lifter ) {
-		pulley.set( driver.getRightY() );
+		pulley.set( lifter.getRightY() != 0 ? lifter.getRightY() : lifter.getLeftY() * 0.5 );
 		
-		double rollOver = 0;
-		if (pot.get() > 0.99 && pulley.get() > 0){
-			rollOver++;
-		}
-		if (pot.get() < 0.01 && pulley.get() < 0){
-			rollOver--;
-		}
 		
-		SmartDashboard.putString("DB/String 9", "POT: " + (pot.get() + rollOver));
+//		double rollOver = 0;
+//		if (pot.get() > 0.99 && pulley.get() > 0){
+//			rollOver++;
+//		}
+//		if (pot.get() < 0.01 && pulley.get() < 0){
+//			rollOver--;
+//		}
+		
+		SmartDashboard.putString("DB/String 9", "POT: " + MathUtils.round(pot.get(), 5));
 	}
 }
