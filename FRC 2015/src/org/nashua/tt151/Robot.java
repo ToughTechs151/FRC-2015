01d68691099ba6befc09fe027749cfd8d1937027
benburtt <bnlbrt@gmail.com>
2015-02-17 19:40:38 -0500

    Added code to test potentiometer rollover
    code added in lifter class under operator control method

--- FRC 2015/src/org/nashua/tt151/Robot.java
+++ FRC 2015/src/org/nashua/tt151/Robot.java
@@ -3,14 +3,11 @@ package org.nashua.tt151;
 import org.nashua.tt151.system.Lifter;
 import org.nashua.tt151.system.MecanumDrive;
 
-import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.SampleRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends SampleRobot {
 	F310 driver = new F310( 0, 0.1 );
 	F310 lifter = new F310( 1, 0.1 );
-	AnalogPotentiometer pot = new AnalogPotentiometer(2);
 	public Robot() {}
 	
 	public void autonomous() {}
@@ -21,7 +18,7 @@ public class Robot extends SampleRobot {
 		while ( isOperatorControl() && isEnabled() ) {
 			driver.query();
 			lifter.query();
-			SmartDashboard.putString( "DB/String 5", "POT: " + MathUtils.round( pot.get(), 5 ) );
+			
 			MecanumDrive.getInstance().operatorControl( driver, lifter );
 			Lifter.getInstance().operatorControl( driver, lifter );
 		}
--- FRC 2015/src/org/nashua/tt151/system/Lifter.java
+++ FRC 2015/src/org/nashua/tt151/system/Lifter.java
@@ -3,12 +3,16 @@ package org.nashua.tt151.system;
 import org.nashua.tt151.F310;
 import org.nashua.tt151.RobotMap;
 
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Lifter extends Subsystem {
 	private static Lifter INSTANCE = null;
 	
 	private Talon pulley = new Talon( RobotMap.PWM.LIFTER_PULLEY );
+	
+	AnalogPotentiometer pot = new AnalogPotentiometer(2);
 	
 	private Lifter() {
 		
@@ -29,5 +33,15 @@ public class Lifter extends Subsystem {
 	@Override
 	public void operatorControl( F310 driver, F310 lifter ) {
 		pulley.set( driver.getRightY() );
+		
+		double rollOver = 0;
+		if (pot.get() > 0.99 && pulley.get() > 0){
+			rollOver++;
+		}
+		if (pot.get() < 0.01 && pulley.get() < 0){
+			rollOver--;
+		}
+		
+		SmartDashboard.putString("DB/String 9", "POT: " + (pot.get() + rollOver));
 	}
 }
